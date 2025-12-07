655be97378b314117b565830e6df8115efb7a3a9
Alexandr Kolosov <Tyuiu.SpirinISKolosovaA@outlook.com>
2025-12-08 04:37:19 +0500

    task4

---
+++ Tyuiu.KolosovAA.Sprint4.Task4.V10/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KolosovAA.Sprint4.Task4.V10.Lib;
+namespace Tyuiu.KolosovAA.Sprint4.Task4.V10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            DataService ds = new DataService();
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите кол-во строк и столбцов через enter");
+            int rows = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int[,] array = new int[rows, length];
+
+            Console.WriteLine("Введите элементы массива");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    array[i, j] = int.Parse(Console.ReadLine());
+                }
+
+            }
+
+            Console.WriteLine("Ваш массив");
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int[,] res = ds.Calculate(array);
+
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    Console.Write(res[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
