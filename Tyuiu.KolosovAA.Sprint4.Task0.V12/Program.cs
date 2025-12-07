bcaedaf297b6239f36c0d752adef1d8ba75b65c2
Alexandr Kolosov <Tyuiu.SpirinISKolosovaA@outlook.com>
2025-12-08 02:26:46 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.KolosovAA.Sprint4.Task0.V12/Program.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.KolosovAA.Sprint4.Task0.V12.Lib;
+
+namespace Tyuiu.KolosovAA.Sprint4.Task0.V12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int res;
+            DataService ds = new DataService();
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                            *");
+            Console.WriteLine("*******************************************************************************");
+            int[] array = { 4, 6, 2, 8, 4, 5, 6, 9, 8, 7 };
+            Console.WriteLine("* Результат:                                                                  *");
+            Console.WriteLine("*******************************************************************************");
+            res = ds.GetMultEvenArrEl(array);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
