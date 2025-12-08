e5f027774892978fc7ed5c35f66e7468ff8c5ec6
Alexandr Kolosov <Tyuiu.SpirinISKolosovaA@outlook.com>
2025-12-08 06:02:04 +0500

    task6

---
+++ Tyuiu.KolosovAA.Sprint4.Task6.V13.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.KolosovAA.Sprint4.Task6.V13.Lib
+{
+    public class DataService : ISprint4Task6V13
+    {
+        public int Calculate(string[] array)
+        {
+            int count = 0;
+
+
+            Array.ForEach(array, element =>
+            {
+                if (element.Length > 4)
+                {
+                    count++;
+                }
+            });
+
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.KolosovAA.Sprint4.Task6.V13.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.Ko.Sprint4.Task6.V13.Lib;
+
+namespace Tyuiu.KolosovAA.Sprint4.Task6.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            string[] cars = ["Ford", "Toyota", "Honda", "Chevrolet", "Mercedes", "BMW", "Audi"];
+
+            int result = ds.Calculate(cars);
+            int wait = 4;
+
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
