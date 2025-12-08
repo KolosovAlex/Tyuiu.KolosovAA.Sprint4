78a9e47e039bf0b14d1fa5f232f95d1a72efc018
Alexandr Kolosov <Tyuiu.SpirinISKolosovaA@outlook.com>
2025-12-08 06:22:29 +0500

    task7

---
+++ Tyuiu.KolosovAA.Sprint4.Task7.V4.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.KolosovAA.Sprint4.Task7.V4.Lib;
+
+namespace Tyuiu.KolosovAA.Sprint4.Task7.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void Calculate_ShouldReturnSumOfOddNumbers()
+        {
+            // Arrange
+            string input = "382976421897";
+            int expectedSum = 36;
+            int n = 3;
+            int m = 4;
+            DataService dataService = new DataService();
+
+            // Act
+            int actualSum = dataService.Calculate(n, m, input);
+
+            // Assert
+            Assert.AreEqual(expectedSum, actualSum);
+        }
+    }
+}
---
+++ Tyuiu.KolosovAA.Sprint4.Task7.V4/Program.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.KolosovAA.Sprint4.Task7.V4.Lib;
+
+DataService dataService = new DataService();
+
+
+Console.WriteLine("************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                     *");
+Console.WriteLine("* 382976421897                                                         *");
+Console.WriteLine("*                                                                      *");
+Console.WriteLine("************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                           *");
+
+string value = "382976421897";
+int n = 3;
+int m = 4;
+
+int sumOfOddNumbers = dataService.Calculate(n, m, value);
+
+Console.WriteLine($"Сумма нечетных чисел: {sumOfOddNumbers}");
+Console.WriteLine("************************************************************************");
+Console.ReadLine();
